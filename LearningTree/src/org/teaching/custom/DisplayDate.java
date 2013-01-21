6684df8fcd30a3feded9635c3dcb60a49ca96fff
Tom <green@email.com>
2013-01-21 16:27:46 -0500

    initial commit

---
+++ LearningTree/src/org/teaching/custom/DisplayDate.java
@@ -0,0 +1,9 @@
+package org.teaching.custom;
+
+
+public class DisplayDate {
+	public static void main(String[] args) {
+        System.out.println("Display Date!"); // Display the string.
+    }
+
+}
---
+++ LearningTree/src/org/teaching/custom/Samples.java
@@ -0,0 +1,9 @@
+package org.teaching.custom;
+
+
+public class Samples {
+	public static void main(String[] args) {
+        System.out.println("Hello world no changes!"); // Display the string.
+    }
+
+}
