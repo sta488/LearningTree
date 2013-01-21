b1304de6dfb980780596009d0a2a6a491a79c447
Sparty <green@email.com>
2013-01-21 16:45:54 -0500

    initial commit

--- LearningTree/src/org/teaching/custom/Samples.java
+++ LearningTree/src/org/teaching/custom/Samples.java
@@ -3,7 +3,7 @@ package org.teaching.custom;
 
 public class Samples {
 	public static void main(String[] args) {
-        System.out.println("Hello world no changes!"); // Display the string.
+        System.out.println("Hello world!"); // Display the string.
     }
 
 }
