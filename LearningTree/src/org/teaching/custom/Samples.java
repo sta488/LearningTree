30ab1748fe5baacc6dcf32424a2d16626beeda5e
Sparty <green@email.com>
2013-01-22 13:26:38 -0500

    commit line or two

--- LearningTree/src/org/teaching/custom/Samples.java
+++ LearningTree/src/org/teaching/custom/Samples.java
@@ -4,6 +4,7 @@ package org.teaching.custom;
 public class Samples {
 	public static void main(String[] args) {
         System.out.println("Hello world!"); // Display the string.
+        System.out.println("Adding new print line or two!"); // Display the string.
     }
 
 }
