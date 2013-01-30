75162bb553d93277fdc65e0fba5d7cf3c9a7b08b
Sparty <green@email.com>
2013-01-30 11:50:17 -0500

    local commit module 1

--- LearningTree/src/org/teaching/custom/Module1.java
+++ LearningTree/src/org/teaching/custom/Module1.java
@@ -1,7 +1,7 @@
 package org.teaching.custom;
 
 
-public class Samples {
+public class Module1 {
 	public static void main(String[] args) {
         System.out.println("Hello world!"); // Display the string.
         System.out.println("Adding new print line or three!"); // Display the string.
