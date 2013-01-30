f3821505f37e7cc43bfe8c63dabf33a12384c717
Sparty <green@email.com>
2013-01-30 12:05:50 -0500

    commit and push topic12

---
+++ LearningTree/src/org/teaching/custom/Topic12.java
@@ -0,0 +1,10 @@
+package org.teaching.custom;
+
+
+public class Topic12 {
+	public static void main(String[] args) {
+        System.out.println("Second Topic file"); // Display the string.
+
+    }
+
+}
