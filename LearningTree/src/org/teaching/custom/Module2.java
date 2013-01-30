7f8c518bbdceb29380e21414223dd2d04b6d9ba5
Sparty <green@email.com>
2013-01-30 11:55:16 -0500

    commit and push module 2

---
+++ LearningTree/src/org/teaching/custom/Module2.java
@@ -0,0 +1,10 @@
+package org.teaching.custom;
+
+
+public class Module2 {
+	public static void main(String[] args) {
+        System.out.println("Hello world!"); // Display the string.
+        System.out.println("Adding new print line or three!"); // Display the string.
+    }
+
+}
